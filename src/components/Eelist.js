9718543c660caaf77296092757c6a0c5bdec5227
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-01-24 14:30:06 +0700

    C5 adding sort function

--- src/components/Eelist.js
+++ src/components/Eelist.js
@@ -1,4 +1,5 @@
 import { Card,CardImg,CardTitle } from 'reactstrap'
+import { Form } from 'react-bootstrap';
 import { useState } from "react"
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/alberto.png'
@@ -6,6 +7,27 @@ function Eelist(props){
     const[selectedEe,setSelectedEe] = useState(null)
     const OnSelectedEe = (staffs)=>{
         setSelectedEe(staffs);
+    }
+    function handleOnCHange(option){
+        if(option == 'Name'){
+            props.staffs.sort(function(a,b){
+                var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+                var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+                if (nameA < nameB) {
+                  return -1;
+                }
+                if (nameA > nameB) {
+                  return 1;
+                }
+
+                // names must be equal
+                return 0;
+            })
+        }else if(option=='Salary'){
+            props.staffs.sort(function(a,b){
+                return a.salaryScale - b.salaryScale;
+            })
+        }
     }
     const Liststaff = props.staffs.map((staffs)=>{
         return(
@@ -23,6 +45,13 @@ function Eelist(props){
     return (
         <div className='container mt-5'>
             <div className='row'>
+            <Form.Select aria-label="Default select example"
+            onChange={handleOnCHange}>
+                <option>Sort by :</option>
+                <option value="1">Name</option>
+                <option value="2">Department</option>
+                <option value="3">Salary</option>
+            </Form.Select>
                 {Liststaff}
             </div>
         </div>
