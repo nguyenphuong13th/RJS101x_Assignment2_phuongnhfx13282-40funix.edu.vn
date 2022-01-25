70fb815c45c1a3e493e904233752823d2a3a07a5
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-01-25 10:36:28 +0700

    C5 adding sorting function to code

--- src/components/Eelist.js
+++ src/components/Eelist.js
@@ -4,13 +4,45 @@ import { useState } from "react"
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/alberto.png'
 function Eelist(props){
+    const[sortedEeList,setsortedEeList]=useState(props.staffs);
     const[selectedEe,setSelectedEe] = useState(null)
+    //Sort by ID
+    // const onSortChangeId = () =>{
+    //    //khi gọi hàm này , thì khởi tạo 1 array mới để giá trị luôn luôn đc render lại
+    //    const copy = [...sortedEeList];
+
+    //     // check xem là cái sort là tăng hay giảm dựa trên cái sortSortedEeLists
+    //     // nếu là true thì sort lại cái copy ở trên vì ở dứoi đã set cái lisst nhân viên = copy rồi
+    //     if(sortSortedEeLists){
+    //         copy.sort(function (a,b){
+    //             return b.id - a.id
+    //         })
+    //     }else{
+    //         copy.sort(function (a,b){
+    //             return a.id - b.id
+    //         })
+    //     }
+    //     //ở đây cũng cần phải set lại cái sortSortedEeListsd để nếu đang là tăng dần thì lần sort sau sẽ là giảm và ngược lại
+    //     setSortsortSortedEeLists(!sortSortedEeLists);
+
+    //    //ở cuối function sẽ set lại cái list nhân viên = cái array mới này
+    //    setsortedEeList(copy);
+    //    khi gọi sort phải tạo lại cái list nhân viên mới
+    //
+    // }
+
+
     const OnSelectedEe = (staffs)=>{
         setSelectedEe(staffs);
     }
-    function handleOnCHange(option){
-        if(option == 'Name'){
-            props.staffs.sort(function(a,b){
+    function handleOnCHange(e){
+        const selectedOption = e.target.value
+        //khi gọi hàm này , thì khởi tạo 1 array mới để giá trị luôn luôn đc render lại
+        const initialEeList = [...sortedEeList]
+        console.log(initialEeList)
+        // Nếu chọn tên thì sẽ lấy ra mảng đã sắp xếp theo tên
+        if(selectedOption == 'Name'){
+            setsortedEeList(initialEeList.sort(function(a,b){
                 var nameA = a.name.toUpperCase(); // ignore upper and lowercase
                 var nameB = b.name.toUpperCase(); // ignore upper and lowercase
                 if (nameA < nameB) {
@@ -22,14 +54,19 @@ function Eelist(props){
 
                 // names must be equal
                 return 0;
-            })
-        }else if(option=='Salary'){
-            props.staffs.sort(function(a,b){
+            }))
+            // Nếu chọn chỉ số lương thì sẽ lấy ra mảng đã sắp xếp theo hệ số lương từ thấp tới cao
+        }else if(selectedOption=='Salary'){
+            console.log(initialEeList.sort(function(a,b){
                 return a.salaryScale - b.salaryScale;
-            })
+            }))
+            setsortedEeList(initialEeList.sort(function(a,b){
+                return a.salaryScale - b.salaryScale;
+            }))
         }
     }
-    const Liststaff = props.staffs.map((staffs)=>{
+    console.log(sortedEeList);
+    const Liststaff = sortedEeList.map((staffs)=>{
         return(
             <div key={staffs.id} onClick={()=>OnSelectedEe(staffs)} className='col-sm-12 col-md-6 col-lg-3 mt-5'>
                 {/* config route when selected invidual selected */}
@@ -45,13 +82,19 @@ function Eelist(props){
     return (
         <div className='container mt-5'>
             <div className='row'>
+                {/* đây là đoạn form bạn sort đúng không ? chút nữa bạn sửa lại css nhé  */}
             <Form.Select aria-label="Default select example"
             onChange={handleOnCHange}>
                 <option>Sort by :</option>
-                <option value="1">Name</option>
-                <option value="2">Department</option>
-                <option value="3">Salary</option>
+                <option value="Name">Name</option>
+                <option value="Department">Department</option>
+                <option value="Salary">Salary</option>
             </Form.Select>
+
+{/* <button style={{ margin: 5, float: "right" }} type="button" className="btn btn-outline-dark " onClick={onSortChangeId}>
+                            ID Sort
+                            <i className="fa fa-sort"></i>
+                        </button> */}
                 {Liststaff}
             </div>
         </div>
