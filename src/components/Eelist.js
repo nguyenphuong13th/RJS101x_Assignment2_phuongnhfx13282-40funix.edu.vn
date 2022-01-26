7f72e7b17704c6b169d12780d3d3dc89272e1c86
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-01-26 11:26:08 +0700

    C5 test

--- src/components/Eelist.js
+++ src/components/Eelist.js
@@ -7,6 +7,8 @@ function Eelist(props){
     const OnSelectedEe = (staffs)=>{
         setSelectedEe(staffs);
     }
+
+
     const Liststaff = props.staffs.map((staffs)=>{
         return(
             <div key={staffs.id} onClick={()=>OnSelectedEe(staffs)} className='col-sm-12 col-md-6 col-lg-3 mt-5'>
